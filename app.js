5031a77ad4da418cbc5561110cb13e73323931c7
rtjshreyd <shreyanshece1041@gmail.com>
2021-07-03 18:53:11 +0530

    Mongo setup and User schema with validation on register

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
 
-//EJS
-app.use(expressLayouts);
-app.set('view engine', ejs);
+// DB Configuration
+const db = require('./config/keys').MongoURI;
 
-// ROutes
+// Connect to MongoDB
+mongoose.connect(db, { useNewUrlParser: true })
+    .then(() => { console.log('MongoDB Connected...');})
+    .catch(err => console.log(err));
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
 
